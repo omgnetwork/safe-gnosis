e0ecd036a701ba5c2b2a531f913c1a4591197d83
Ino Murko <2582555+InoMurko@users.noreply.github.com>
2021-12-14 13:03:05 +0100

    bump all services script

---
+++ bump.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+ROOT=$(pwd)
+echo "safe-transaction-service at tag:" && \
+cd boba_ops/safe-transaction-service && \
+git fetch && git describe --tags --abbrev=0 && \
+git rev-parse HEAD
+cd $ROOT
+echo -e "\nsafe-config-service at tag:" && \
+cd boba_ops/safe-config-service && \
+git fetch && git describe --tags --abbrev=0 && \
+git rev-parse HEAD
+cd $ROOT
+echo -e "\nsafe-client-gateway at tag:" && \
+cd boba_ops/safe-client-gateway && \
+git fetch && git describe --tags --abbrev=0 && \
+git rev-parse HEAD
