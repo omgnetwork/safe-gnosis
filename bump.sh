49edc648af0a263625b5fadf1f06942042c9fb64
Ino Murko <2582555+InoMurko@users.noreply.github.com>
2022-01-03 13:34:17 +0100

    safe react to dev tip

--- bump.sh
+++ bump.sh
@@ -3,19 +3,23 @@ ROOT=$(pwd)
 echo "safe-transaction-service at tag:" && \
 cd boba_ops/safe-transaction-service && \
 git fetch && git describe --tags --abbrev=0 && \
+git checkout tags/$(git describe --tags --abbrev=0) && \
 git rev-parse HEAD
 cd $ROOT
 echo -e "\nsafe-config-service at tag:" && \
 cd boba_ops/safe-config-service && \
 git fetch && git describe --tags --abbrev=0 && \
+git checkout tags/$(git describe --tags --abbrev=0) && \
 git rev-parse HEAD
 cd $ROOT
 echo -e "\nsafe-client-gateway at tag:" && \
 cd boba_ops/safe-client-gateway && \
 git fetch && git describe --tags --abbrev=0 && \
+git checkout tags/$(git describe --tags --abbrev=0) && \
 git rev-parse HEAD
 cd $ROOT
 echo -e "\nsafe-react at tag:" && \
 cd boba_ops/safe-react && \
 git fetch && git describe --tags --abbrev=0 && \
+git checkout tags/$(git describe --tags --abbrev=0) && \
 git rev-parse HEAD
